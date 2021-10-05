6e73fe8dd2040617377468a666a77f80dd4e5866
Evgeny Morozov <eevgmor@gmail.com>
2021-10-05 19:46:20 +0300

    Sprint_2.Final_v02

--- homework.py
+++ homework.py
@@ -15,15 +15,14 @@ class Calculator:
         self.records = []
 
     def add_record(self, record):
-        """Receives object of class Record and adds a record"""
         self.records.append(record)
 
     def get_week_stats(self):
         """Calculates stats for past 7 days"""
         today = dt.date.today()
-        return sum(self.records.amount for self.records in self.records
-                   if ((today - self.DAYS_IN_WEEK) < self
-                       .records.date <= today))
+        week = today - self.DAYS_IN_WEEK
+        return sum(record.amount for record in self.records
+                   if (week < record.date <= today))
 
     def get_today_stats(self):
         """Calculates stats for current day"""
@@ -37,7 +36,6 @@ class Record:
     Class Record receives data and saves it.
     Input - amount, comment, date
     """
-    # Here we define input date format as DMY
     DATE_FORMAT = '%d.%m.%Y'
 
     def __init__(self, amount, comment, date=None):
@@ -53,7 +51,6 @@ class CashCalculator(Calculator):
     """
     Class CashCalculator contains method get_today_cash_remained
     Limit is taken from parent class Calculator.
-    Converts to RUB, EUR, USD
     """
     USD_RATE = 60.0
     EURO_RATE = 70.0
@@ -63,34 +60,30 @@ class CashCalculator(Calculator):
         'eur': ('Euro', EURO_RATE)
     }
 
-    CURRENCY_UNKNOWN = 'Валюта неизвестна'
-
-    CASH_OVER_LIMIT = ('Денег нет, держись: твой долг - '
-                       '{cash_debt_format} {currency_name}')
-    CASH_UNDER_LIMIT = ('На сегодня осталось '
-                        '{cash_remained_format} {currency_name}')
-    CASH_LIMIT_REACHED = 'Денег нет, держись'
+    CURRENCY_UNKNOWN = 'Значение {name} недопустимо'
+    OVER_LIMIT = ('Денег нет, держись: твой долг - {cash_debt} {name}')
+    UNDER_LIMIT = ('На сегодня осталось {cash_remained} {name}')
+    LIMIT_REACHED = 'Денег нет, держись'
 
     def get_today_cash_remained(self, currency):
         """
         Calculates remaining cash for today.
         Receives currency and converts to it
         """
-
         if currency not in self.CURRENCIES:
-            return self.CURRENCY_UNKNOWN
-        today_cash_remained = round((self.limit - self.get_today_stats()
-                                     ) / self.CURRENCIES[currency][1], 2)
+            raise ValueError(self.CURRENCY_UNKNOWN.format(name=currency))
+        today_cash_remained = self.limit - self.get_today_stats()
+        name, rate = self.CURRENCIES[currency]
         if today_cash_remained == 0:
-            return self.CASH_LIMIT_REACHED
+            return self.LIMIT_REACHED
+        today_cash_remained = round(today_cash_remained / rate, 2)
         if today_cash_remained > 0:
-            return self.CASH_UNDER_LIMIT.format(
-                cash_remained_format=today_cash_remained,
-                currency_name=self.CURRENCIES[currency][0])
-        else:
-            return self.CASH_OVER_LIMIT.format(
-                cash_debt_format=abs(today_cash_remained),
-                currency_name=self.CURRENCIES[currency][0])
+            return self.UNDER_LIMIT.format(
+                cash_remained=today_cash_remained,
+                name=name)
+        return self.OVER_LIMIT.format(
+            cash_debt=abs(today_cash_remained),
+            name=name)
 
 
 class CaloriesCalculator(Calculator):
@@ -99,16 +92,14 @@ class CaloriesCalculator(Calculator):
     Limit is taken from parent class Calculator.
     """
 
-    CALORIES_OVER_LIMIT = 'Хватит есть!'
-    CALORIES_UNDER_LIMIT = ('Сегодня можно съесть что-нибудь ещё, '
-                            'но с общей калорийностью '
-                            'не более {calories_remained_format} кКал')
+    OVER = 'Хватит есть!'
+    UNDER = ('Сегодня можно съесть что-нибудь ещё, '
+             'но с общей калорийностью '
+             'не более {calories} кКал')
 
     def get_calories_remained(self):
         """Calculcates remained calories for today"""
-        calories_remained = (self.limit - self.get_today_stats())
+        calories_remained = self.limit - self.get_today_stats()
         if calories_remained > 0:
-            return self.CALORIES_UNDER_LIMIT.format(
-                calories_remained_format=calories_remained)
-        else:
-            return self.CALORIES_OVER_LIMIT
+            return self.UNDER.format(calories=calories_remained)
+        return self.OVER
